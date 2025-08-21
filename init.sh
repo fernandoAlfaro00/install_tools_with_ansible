0223820e9be85f38985a2dccc053da5b74648555
akira <akira@debian>
2025-08-20 22:59:58 -0400

    Update docs and playbook for debian 12

--- init.sh
+++ init.sh
@@ -2,4 +2,7 @@
 sudo apt  install curl -y
 python3 -m venv venv_ansible
 venv_ansible/bin/pip3 install ansible
-venv_ansible/bin/ansible-playbook --ask-become-pass main.yaml
+source venv_ansible/bin/activate
+echo -e "====================================================================="
+echo -e "\033[0;31mIntroduce tu password sudo para avanzar con la instalacion\033[0m"
+ansible-playbook --ask-become-pass main.yaml --tags kvm-qemu
